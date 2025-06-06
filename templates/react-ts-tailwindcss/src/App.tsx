44005b23fd8e06a269f1021260cf6a7fffe75aa7
Ivan Luengo <ivluengo@gmail.com>
2025-06-06 09:19:28 +0200

    add react-ts-tailwind to templates

---
+++ templates/react-ts-tailwindcss/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto my-8 px-4">
+      <h1 className="text-3xl font-bold">
+        App con React, TypeScript y TailwindCSS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
